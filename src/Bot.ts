2f8e1e0c3fc8b6efd4e7e4ea0c6bcc85bfbbf99c
matiasfacello <matiasfacello@gmail.com>
2023-04-03 09:35:41 -0300

    feat: adds interaction to commands

--- src/Bot.ts
+++ src/Bot.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 import { config } from "dotenv";
 config();
 
-import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { Client, Collection, GatewayIntentBits, Events } from "discord.js";
 
 const bot = new Client({
   intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildPresences],
@@ -29,6 +29,29 @@ for (const folder of commandFolders) {
     }
   }
 }
+
+bot.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const client = interaction.client as ClientType;
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+    } else {
+      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+    }
+  }
+});
 
 bot.login(process.env.BOT_TOKEN);
 
