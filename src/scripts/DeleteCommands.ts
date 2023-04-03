3223635a432e3727dd465688a55a06af238a12e3
matiasfacello <matiasfacello@gmail.com>
2023-04-03 09:08:10 -0300

    feat: adds delete and deploy cmds scripts

---
+++ src/scripts/DeleteCommands.ts
@@ -0,0 +1,9 @@
+import { REST, Routes } from "discord.js";
+import { config } from "dotenv";
+config();
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+
+rest
+  .put(Routes.applicationCommands(process.env.APP_ID), { body: [] })
+  .then(() => console.log("Successfully deleted all application commands."))
+  .catch(console.error);
---
+++ src/scripts/DeployCommands.ts
@@ -0,0 +1,33 @@
+import { REST, Routes } from "discord.js";
+import * as fs from "fs";
+import * as path from "path";
+import { config } from "dotenv";
+config();
+
+const commands = [];
+const foldersPath = path.join(__dirname, "..", "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const data = (await rest.put(Routes.applicationCommands(process.env.APP_ID), { body: commands })) as any[];
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
