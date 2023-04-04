6c7a883693af40ec9de4048cf8880a6ff6486d21
matiasfacello <matiasfacello@gmail.com>
2023-04-03 21:55:55 -0300

    fix: removes unnecesary intents

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,7 +6,7 @@ config();
 import { Client, GatewayIntentBits } from "discord.js";
 
 const bot = new Client({
-  intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildPresences],
+  intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds],
 }) as ClientType;
 
 bot.login(process.env.BOT_TOKEN);
